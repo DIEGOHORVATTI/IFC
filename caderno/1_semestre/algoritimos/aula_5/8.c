2d676eabe1157ffde214635d2fb380566e9ec24a
DIEGOHORVATTI <d.horvattid@gmail.com>
2022-04-12 19:55:01 -0400

    fix: aula 8 c

--- caderno/1_semestre/algoritimos/aula_5/8.c
+++ caderno/1_semestre/algoritimos/aula_5/8.c
@@ -1,3 +1,4 @@
+//compilador "gcc ./8.c -o ./exe/8.exe && ./exe/8.exe"
 /*
   Em uma eleição presidencial existem quatro candidatos. Os votos são informados através de códigos. Os dados utilizados para a contagem dos votos obedecem à seguinte codificação:
   1,2,3,4 = voto para os respectivos candidatos;
