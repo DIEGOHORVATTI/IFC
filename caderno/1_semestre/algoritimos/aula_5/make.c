6df7d63f734a53b08cb2ebc8f206b5b414dec346
DIEGOHORVATTI <d.horvattid@gmail.com>
2022-05-03 03:26:46 -0400

    add: aula_5 atualização de make

--- caderno/1_semestre/algoritimos/aula_5/make.c
+++ caderno/1_semestre/algoritimos/aula_5/make.c
@@ -1,6 +1,29 @@
 #include <stdlib.h>
 
-int make(){ 
-	system("bash ./make.sh"); 
+int make(){
+		
+	int make=0;
+	printf(" ver lista(y/n)?: ");
+	scanf("%s", &make);
+
+	//y = 121
+	//Y = 89
+	//n = 110
+	//N = 
+
+	printf("%d\n", make);
+
+	/*switch(make){
+		case 121:
+		case 89:
+			system("bash ./make.sh"); break;
+			return(0);
+		case 110:
+		case 
+			return(0); break;
+		default: 
+			return(0);
+	}*/
+
 	return(0);
 }
