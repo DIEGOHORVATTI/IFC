eb3f08b8aed53be7917114a12aae50ca579538ce
DIEGOHORVATTI <d.horvattid@gmail.com>
2022-05-26 22:05:29 -0400

    add: 10.c

--- caderno/1_semestre/algoritimos/aula_7/10.c
+++ caderno/1_semestre/algoritimos/aula_7/10.c
@@ -1,5 +1,6 @@
 /*
-  Escreva um algoritmo que lê uma matriz M(5,5) e calcula as somas:
+  Escreva um algoritmo que lê uma matriz M(5,5) e 
+  calcula as somas:
   da linha 4 de M.
   da coluna 2 de M.
   da diagonal principal.
@@ -7,72 +8,61 @@
   de todos os elementos da matriz.
   Escreva estas somas e a matriz.
 */
+#include <stdio.h> 
+#define MAX_LEITURA 5
 
-#include <stdio.h>
-#include <stdlib.h>
+int main(void){
 
-int main(int argc, const char * argv[]) {
+  int matrix[MAX_LEITURA][MAX_LEITURA];
+  for (int j = 0; j < MAX_LEITURA; ++j){
+    for (int i = 0; i < MAX_LEITURA; ++i){
+      matrix[j][i];
+      printf(" %d", matrix[j][i]);
+    }    
+    printf("\n");
+  }
 
-#define M mat; 
+  /* int mat_1[5][5] = { 3, 8, 1, 6, 2, 0, 8, 3, 9};
+  for (int j = 0; j < 3; ++j){
+    for (int i = 0; i < 3; ++i){
+      printf(" %d", mat_1[j][i]);
+    }    
+    printf("\n");
+  }
 
-int mat[5][5]={0}; 
-int i=0, j=0;
-int SL4=0, SC2=0;
-int diap=0, dias=0;
-int soma=0;
+  //soma linha 2
+  int soma_linha_2=0;
+  for (int i = 0; i < 3; ++i){
+    soma_linha_2+=mat_1[1][i];
+  }    
+  printf("\n soma linha 2: %d \n", soma_linha_2);
 
-for (i=0; i<5; i++) {
-for (j=0; j<5; j++) { 
-printf(“Digite valora para M[%d][%d]: “, i, j); 
-scanf(“%d”, &mat[i][j]); 
-}
-}
-printf(“\n”);
+  //soma coluna 2
+  int soma_coluna_2=0;
+  for (int i = 0; i < 3; ++i){
+    soma_coluna_2+=mat_1[i][1];
+  }    
+  printf("\n soma coluna 2: %d \n", soma_coluna_2);
+  
+  //soma diagonal
+  int soma_diagonal=0;
+  for (int i = 0; i < 3; ++i){
+    soma_diagonal+=mat_1[i][i];
+    printf(" %d", mat_1[i][i]);
+  }
+  printf("\n soma diagonal: %d \n\n", soma_diagonal); 
 
+  //soma diagonal segundaria
+  int soma_diagonal_inversa=0;
+  for (int i = 2; i >= 0; --i){
+    soma_diagonal_inversa+=mat_1[i][i];
+    printf(" %d", mat_1[i][i]);
+  }
+  printf("\n soma diagonal inversa: %d \n\n", soma_diagonal_inversa); 
 
-for (i=0; i<5; i++) { 
-SL4 = SL4 + mat[3][i]; 
-}
-printf(“Soma da linha 4: %d”, SL4);
-printf(“\n”);
-
-
-for (j=0; j<5; j++) { 
-SC2 = SC2 + mat[j][1]; 
-}
-printf(“Soma da coluna 2: %d”, SC2); 
-printf(“\n”);
-
-
-
-for(i=0;i<5;i++) { 
-for(j=0;j<5;j++) {
-if (i==j) { 
-diap=diap+mat[i][j];
-}
-}
-}
-printf(“\nA soma da diagonal principal eh: %d”, diap);
-printf(“\n”);
+  //soma diagonal segundaria
+  printf("\n soma zuada: %d %d %d \n\n", mat_1[0][2], mat_1[1][1], mat_1[2][0] );  */
 
 
-for(i=0;i<5;i++) { 
-for(j=0;j<5;j++) {
-if ( j == ( 5 – 1 – i )) { 
-dias=dias+mat[i][j];
-}
-}
-}
-printf(“\nA soma da diagonal secundaria eh: %d”, dias);
-printf(“\n”);
-
-for(i=0;i<5;i++) { 
-for(j=0;j<5;j++) {
-soma = soma + mat[i][j]; 
-}
-}
-printf(“\nA soma da matriz eh: %d”, soma); 
-printf(“\n”);
-
-return 0;
+  return(0);
 }
