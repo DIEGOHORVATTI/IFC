5ee4116b8a3167000ca3eb98343db03c1e193960
DIEGOHORVATTI <d.horvattid@gmail.com>
2022-05-27 07:26:45 -0400

    add: 15.c

--- caderno/1_semestre/algoritimos/aula_7/15.c
+++ caderno/1_semestre/algoritimos/aula_7/15.c
@@ -20,4 +20,39 @@
 #include <stdio.h>
 #define MAX_MATRIX 7
 
-int matrix[MAX_MATRIX][MAX_MATRIX], vetor_[];
+int main(void){
+    
+  int cont=0, matrix[MAX_MATRIX][MAX_MATRIX], vetor_bd[MAX_MATRIX*MAX_MATRIX]={  0, 2, 11, 6, 15, 11, 1, 2, 0, 7, 12, 4, 2, 15, 11, 7, 0, 11, 8, 3, 13, 6,12, 11, 0, 10, 2, 1, 15, 4, 8, 10, 0, 5, 13, 11, 2, 3, 2, 5, 0, 14, 1,15, 13, 1, 13, 14, 0};
+
+  for(int j=0; j < MAX_MATRIX; j++){
+    for(int i=0; i < MAX_MATRIX; i++){
+      matrix[i][j]=vetor_bd[cont];
+      cont++;
+    }
+  }
+
+  for(int j=0; j < MAX_MATRIX; j++){
+    for(int i=0; i < MAX_MATRIX; i++){
+      if( matrix[i][j] >= 10 ){
+        printf(" %d", matrix[i][j]);
+      }else{
+        printf("  %d", matrix[i][j]);
+      }
+    }
+    printf("\n");
+  }
+
+  printf("\n---------------\n");
+
+  for (int i=0; i < MAX_MATRIX ; i++){
+    printf(" %d", matrix[i][i]);
+    printf(" %d", matrix[i][i-1]);
+    printf(" %d", matrix[i][i-2]);
+    printf(" %d", matrix[i][i-3]);
+    printf(" %d", matrix[i][i-4]);
+    printf("\n");
+  }
+
+
+  return(0);
+}
