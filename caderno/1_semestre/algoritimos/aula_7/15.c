f845cca79192114d3c3faa4224afe2e350fed7fe
DIEGOHORVATTI <d.horvattid@gmail.com>
2022-05-31 18:28:02 -0400

    fix: 4.c

--- caderno/1_semestre/algoritimos/aula_7/15.c
+++ caderno/1_semestre/algoritimos/aula_7/15.c
@@ -23,6 +23,7 @@
 int main(void){
     
   int cont=0, matrix[MAX_MATRIX][MAX_MATRIX], vetor_bd[MAX_MATRIX*MAX_MATRIX]={  0, 2, 11, 6, 15, 11, 1, 2, 0, 7, 12, 4, 2, 15, 11, 7, 0, 11, 8, 3, 13, 6,12, 11, 0, 10, 2, 1, 15, 4, 8, 10, 0, 5, 13, 11, 2, 3, 2, 5, 0, 14, 1,15, 13, 1, 13, 14, 0};
+  char cidade_x, cidade_y;
 
   for(int j=0; j < MAX_MATRIX; j++){
     for(int i=0; i < MAX_MATRIX; i++){
@@ -43,15 +44,41 @@ int main(void){
   }
 
   printf("\n---------------\n");
+  
+  int x, y;
+  for(int j=0; j < MAX_MATRIX; j++){
+    for(int i=0; i < MAX_MATRIX; i++){
+      printf("\n %s", "Digite a cidade X: ");
+      scanf("%s", &cidade_x);
+      printf("\n %s", "Digite a cidade Y: ");
+      scanf("%s", &cidade_y);
+      
+      if( cidade_x == 'a'){ x=0; }
+      else if( cidade_x == 'b'){ x=1; }
+      else if( cidade_x == 'c'){ x=2; }
+      else if( cidade_x == 'd'){ x=3; }
+      else if( cidade_x == 'e'){ x=4; }
+      else if( cidade_x == 'f'){ x=5; }
 
-  for (int i=0; i < MAX_MATRIX ; i++){
+      if( cidade_y == 'a'){ y=0; }
+      else if( cidade_y == 'b'){ y=1; }
+      else if( cidade_y == 'c'){ y=2; }
+      else if( cidade_y == 'd'){ y=3; }
+      else if( cidade_y == 'e'){ y=4; }
+      else if( cidade_y == 'f'){ y=5; }
+
+      printf(" %d", matrix[x][y]);
+    }
+  }
+
+  /* for (int i=0; i < MAX_MATRIX ; i++){
     printf(" %d", matrix[i][i]);
     printf(" %d", matrix[i][i-1]);
     printf(" %d", matrix[i][i-2]);
     printf(" %d", matrix[i][i-3]);
     printf(" %d", matrix[i][i-4]);
     printf("\n");
-  }
+  } */
 
 
   return(0);
--- caderno/1_semestre/algoritimos/aula_7/4.c
+++ caderno/1_semestre/algoritimos/aula_7/4.c
@@ -4,51 +4,53 @@ Em seguida,
 copiar os elementos 
 * pares divisíveis por 3 para o vetor X e os 
 * ímpares divisíveis por 5 para o vetor Y.
-0 -> par
-1 -> impar
+0 => par
+1 => impar
+*/
+
+/* 
+	porque eu passei o vetor de X, Y como 0 e eles foram alocados dinamicamente? 
 */
 
 #include <stdio.h>
+#define TECLADO 4
 
 int main(void){
 
-	int num, x, y, teclado, X[0], Y[0], N[0];
-	x=y=0;
+	int N[TECLADO], X[0], Y[0],cont_x, cont_y, x, y;
+	x=y=cont_x=cont_y=0;
 
-	teclado = 12; // numeros de matriz do piloto
-
-	//adicionar numeros ao vetor N
-	for (int i=1; i <= teclado; ++i){
-
-		//printar e alocar valor digitado em num
-		printf("%s[%d]: ", " Digite um numero", i); scanf("%d", &num);
-		
-		N[i] = num; //adicionar i como posição de vetor e num como vetor
-
-		//  0 == par    |  0 == divisivel por 3
-		if( N[i]%2 == 0 && N[i]%3 == 0){
-			x++;
-			X[x] = N[i]; 
-		}
-		
-		//  !=0 impar   |  0 == divisivel por 5
-		if( N[i]%2 != 0 && N[i]%5 == 0){
-			y++;
-			Y[y] = N[i];
-		}
-
+	for(int i=0; i < TECLADO; i++){
+		printf(" Digite um numero[%d]: ", i+1);
+		scanf("%d", &N[i]); //atualizar os indices do vetor N
 	}
 
-	//printar lista X
-	printf("\n");
-	for (int i=1; i <= x; ++i){
-		printf(" X[%d]: %d\n", i, X[i]);
+	//atualizar X e Y
+	for( int i=0; i < TECLADO; i++){
+		if( ((N[i]%2)==0) ){     //Se for par
+			if( ((N[i]%3)==0 )){   //Se for divisivel por 3
+				printf("\n N[%d] = %d -> É par e divisivel por 3 ", i+1, N[i]);
+				X[x]=N[i];     //Valor de X é atualizado pelo N
+				x+=1;          //cont_x + cont_x+1 | cont x é o incremento de todas as passagens pelo if de divisão por 3
+			}
+		}
+		else{                    //Se não for par
+			if( ((N[i]%5)==0 )){   //Se for divisivel por 5
+				printf("\n N[%d] = %d -> É divisivel por 5 ", i+1, N[i]);
+				Y[y]=N[i];
+				y+=1;
+			}
+		}
+	}
+	
+	printf("\n\n");
+	for(int i=0; i < x; i++){
+		printf(" X[%d] -> %d\n", i+1, X[i]);
 	}
 
-	//printar lista Y
 	printf("\n");
-	for (int i=1; i <= y; ++i){
-		printf(" Y[%d]: %d\n", i, Y[i]);
+	for(int i=0; i < y; i++){
+		printf(" Y[%d] -> %d\n", i+1, Y[i]);
 	}
 
 	return(0);
