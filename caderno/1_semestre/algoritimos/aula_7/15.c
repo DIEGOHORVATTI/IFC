d398cd06c1f6d93aa16cb3d3a7b6d250fea6b439
DIEGOHORVATTI <d.horvattid@gmail.com>
2022-06-02 07:49:37 -0400

    edit: remove readme

--- caderno/1_semestre/algoritimos/aula_7/15.c
+++ caderno/1_semestre/algoritimos/aula_7/15.c
@@ -9,7 +9,7 @@
   até o momento que ele fornecer duas cidades iguais (origem e destino).
 
   Desafios:
-  Faça um algoritmo que mostre a tabela sem repetições (apenas o triângulo superior ou o inferior).
+  Façhttps://i.imgur.com/B3sfhJ2.pnga um algoritmo que mostre a tabela sem repetições (apenas o triângulo superior ou o inferior).
 
   Faça um algoritmo que permita ao usuário informar várias cidades, 
   até inserir uma cidade X, 
@@ -25,13 +25,15 @@ int main(void){
   int cont=0, matrix[MAX_MATRIX][MAX_MATRIX], vetor_bd[MAX_MATRIX*MAX_MATRIX]={  0, 2, 11, 6, 15, 11, 1, 2, 0, 7, 12, 4, 2, 15, 11, 7, 0, 11, 8, 3, 13, 6,12, 11, 0, 10, 2, 1, 15, 4, 8, 10, 0, 5, 13, 11, 2, 3, 2, 5, 0, 14, 1,15, 13, 1, 13, 14, 0};
   char cidade_x, cidade_y;
 
+  //atribui valores do vetor a matriz
   for(int j=0; j < MAX_MATRIX; j++){
     for(int i=0; i < MAX_MATRIX; i++){
       matrix[i][j]=vetor_bd[cont];
       cont++;
     }
   }
-
+  
+  //formatar saida de printf
   for(int j=0; j < MAX_MATRIX; j++){
     for(int i=0; i < MAX_MATRIX; i++){
       if( matrix[i][j] >= 10 ){
