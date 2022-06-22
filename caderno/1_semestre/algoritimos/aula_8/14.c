b33079eb62d1fa1ce47ddfcf959013a4135de6d3
DIEGOHORVATTI <d.horvattid@gmail.com>
2022-06-22 06:25:40 -0400

    add: 14.c

--- caderno/1_semestre/algoritimos/aula_8/14.c
+++ caderno/1_semestre/algoritimos/aula_8/14.c
@@ -9,30 +9,70 @@
 #include <stdio.h>
 #define MAX_POSITION 10
 
-int main(void){
-  
-  int matriz_rest_quests[MAX_POSITION][MAX_POSITION];
-  char vetor_gabarito[MAX_POSITION]={ 'b', 'c', 'b', 'a', 'c', 'd', 'c', 'a', 'c', 'd'};
+int matriz_rest_quests[MAX_POSITION][MAX_POSITION], pontuacao[MAX_POSITION];
+char vetor_gabarito[MAX_POSITION]={ 'b', 'c', 'b', 'c', 'b', 'a', 'c', 'd', 'c', 'a'};
 
-
+int inserir_resposta(){
   for(int j=0; j < MAX_POSITION; j++){
     for(int i=0; i < MAX_POSITION; i++){
-      printf(" Resposta[%d] de aluno[%d], (a,b,c,d)", i+1, j+1);
+      printf("Aluno[%d], resposta[%d], (a,b,c,d): ", i+1, j+1);
       scanf("%s", &matriz_rest_quests[i][j]);
     }
     printf("\n");
   }
+}
 
+
+int verifica_resposta(int i, int j){
+  if(matriz_rest_quests[i][j] == vetor_gabarito[i]){ return(1); }
+  else{ return(0); }
+}
+
+int debug_quests(){
   for(int j=0; j < MAX_POSITION; j++){
     for(int i=0; i < MAX_POSITION; i++){
-      if(matriz_rest_quests[i][j] == vetor_gabarito[i]){
-        printf("\n Resposta[%d] de aluno[%d], Correta: ", i+1, j+1);
-      }else{
-        printf("\n Resposta[%d] de aluno[%d], Errada: ", i+1, j+1);
+      if( verifica_resposta(i ,j) == 1 ){
+        printf("\nAluno[%d], resposta[%d], [Correta]", i+1, j+1 );
+      } else{
+        printf("\nAluno[%d], resposta[%d], [Errada]", i+1, j+1 );
       }
     }
-    printf("\n");
+    printf("\n---------------------------------");
   }
+}
+
+int calcula_pontuacao(){
+  for(int j=0; j < MAX_POSITION; j++){
+
+    int soma=0;
+    for(int i=0; i < MAX_POSITION; i++){
+      if( verifica_resposta(i ,j) == 1 ){
+        soma+=1;
+      } else{
+        soma+=0;
+      }
+    }
+    pontuacao[j]=soma;
+  }
+}
+
+int debug_pontuacao(){
+
+  calcula_pontuacao();
+  printf("\n");
+  for(int i=0; i < MAX_POSITION; i++){
+    printf("\nPontuação aluno[%d]: %d", i+1, pontuacao[i] );
+  }
+}
+
+int main(void){
+
+  inserir_resposta();
+
+  debug_quests();
+
+  debug_pontuacao();
+
 
   return(0);
 }
