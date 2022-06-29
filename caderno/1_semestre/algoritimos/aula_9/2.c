b217e9d6c8177d2310ed969316e7ed0b0739962f
DIEGOHORVATTI <d.horvattid@gmail.com>
2022-06-29 01:24:34 -0400

    add: 2.c

--- caderno/1_semestre/algoritimos/aula_9/2.c
+++ caderno/1_semestre/algoritimos/aula_9/2.c
@@ -38,9 +38,8 @@ não possuem nenhum dígito igual a zero.
 #include <time.h>
 #include <string.h>
 
-#define QNT 1
-#define NOME 100
-#define CHASSI 120
+#define NOME 20
+#define CHASSI 20
 #define MAX_PLACA_LETRA 3 // 3 caracteres de letra da placa
 #define ALFABETO 26  // maximo de caracteres no alfabeto brasileiro
 #define MAX_NOME 100 // tamnaho máximo de caracteres para formar um nome
@@ -88,7 +87,7 @@ int retornoPlaca_letra(){
   return( vetLetras[ retornoPlaca_numero() ] );
 } 
 
-// função que retorna o nome do proprietário randomicamente
+// função que retorna o nome do proprietário rândomicamente
 char* retornaNome(){
   char* string = (char*) malloc( ( sizeof(char)* sizeof( string ) ) );
 
@@ -99,7 +98,7 @@ char* retornaNome(){
   return ( string );
 }
 
-// função que retorna o nome do proprietário randomicamente
+// função que retorna o chassis do veiculo
 char* retornaChassi(){
   char* string = (char*) malloc( ( sizeof(char)* sizeof( string ) ) );
 
@@ -110,28 +109,7 @@ char* retornaChassi(){
   return ( string );
 }
 
-// retorna [0, 1, 2] aleatóriamente
-int retornoCombustivel(){ 
-  return (rand() % 3); 
-} 
-
-// retorna [0, 1] aleatóriamente
-int retornaAno(){ 
-  return (rand() % 2023); 
-} 
-
-// retorna [0, 1, 2, 3, 4] aleatóriamente
-int retornoModelo(){ 
-  return (rand() % 10); 
-} 
-
-// retorna [0, 1] aleatóriamente
-int retornoCor(){ 
-  return (rand() % 2); 
-} 
-
-
-// função que retorna as 3 primeiras letras da placa randomicamente
+// função que retorna as 3 primeiras letras da placa rândomicamente
 char* retornaPlaca_3(){
   char* vet_placa = (char*) malloc( ( sizeof(char)* sizeof( vet_placa ) ) );
   
@@ -142,23 +120,24 @@ char* retornaPlaca_3(){
   return ( vet_placa );
 }
 
-// função que retorna os 4 primeiros numeros da placa randomicamente
-int retornaPlaca_4(){
-  return( rand() % 9999 );
-}
-
 sVeiculo atribuirVeiculo(){
   sVeiculo veiculo;
 
+  // nome aleatório
   char* nome = retornaNome();
   strcpy(veiculo.proprietario.nome, nome);
 
-  veiculo.placa.numero = retornaPlaca_4();
-  veiculo.combustivel = retornoCombustivel();
-  veiculo.modelo = retornoModelo();
-  veiculo.cor = retornoCor();
-  veiculo.ano = retornaAno();
+  veiculo.placa.numero = ( rand() % 9999 );  // retorna os 4 primeiros numeros da placa rândomicamente
+  veiculo.combustivel  = ( rand() % 3 );     // retorna [0, 1, 2],['álcool', 'diesel', 'gasolina'] aleatóriamente
+  veiculo.modelo       = ( rand() % 10 );    // retorna [0, 1, 2, 3, 4] aleatóriamente
+  veiculo.cor          = ( rand() % 2 );     // retorna [0, 1],[branco, preto] aleatóriamente
+  veiculo.ano          = ( rand() % 2023 );  // retorna um ano aleatóriamente
 
+  // chassi aleatório
+  char* chassi = retornaChassi();
+  strcpy(veiculo.chassi, chassi);
+
+  // letras de placa aleatórios
   char* placaLetra = retornaPlaca_3();
   strcpy(veiculo.placa.letra, placaLetra);
 
@@ -170,8 +149,10 @@ void imprimirVeiculos(sVeiculo veiculo){
   printf("Proprietario: %s", veiculo.proprietario.nome);
   printf("\nModelo: %d", veiculo.modelo);
   printf("\nCor: %d", veiculo.cor);
+  printf("\nAno: %d", veiculo.ano);
   printf("\nCombustivel: %d", veiculo.combustivel);
   printf("\nPlaca: %s-%d", veiculo.placa.letra, veiculo.placa.numero);
+  printf("\nChassis: %s", veiculo.chassi);
 }
 
 /*
@@ -217,21 +198,17 @@ int main(){
 
   srand(time(NULL)); // semente aleatória mudada pela hora do sistema
   
-  sVeiculo veiculo[5];
+  int loop = 5;
 
-  for (int i=0; i < 3; i++){
+  sVeiculo veiculo[ loop ];
+
+  for (int i=0; i < loop; i++){
     veiculo[i] = atribuirVeiculo();
   }
 
-  for (int i=0; i < 3; i++){
+  for (int i=0; i < loop; i++){
     imprimirVeiculos( veiculo[i] );
   }
-  
-
-  /* sVeiculo veiculo[10];
-
-  atribuirVeiculo();
-  imprimirVeiculos( veiculo[0] ); */
 
 	return 0;
 }
