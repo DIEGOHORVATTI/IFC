487ac5a8d1112800b6b0f46ebf377df72bf9ab66
DIEGOHORVATTI <d.horvattid@gmail.com>
2022-08-29 21:45:35 -0400

    add: tarefa 12

---
+++ caderno/2_semestre/java/T12.java
@@ -0,0 +1,41 @@
+/* 
+ * Faça um programa que leia um conjunto não determinado de valores, 
+ * um de cada vez, e escreva uma tabela com cabeçalho, 
+ * que deve ser repetido a cada vinte linhas. 
+ * 
+ * A tabela deverá conter o valor lido, seu cubo e sua raiz quadrada. 
+ * Finalize quando a entrada for um número negativo ou 0.
+ */
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class T12 {
+  public static void main(String[] args) {
+    int LINHAS = 20;
+    int teclado, i, valores[] = new int[LINHAS];
+    i=0;
+    teclado=1;
+    Scanner tecladoLeitura = new Scanner(System.in);
+    
+    while(true){
+
+      System.out.print("["+ (i+1) +"]: >> ");
+      teclado = tecladoLeitura.nextInt();
+      if (teclado <= 0) { break; }
+      valores[i] = teclado;
+      
+      i++;
+      if(i == LINHAS){
+        System.out.println();
+        for (int j=0; j < LINHAS; j++) {
+          System.out.println("["+(j+1)+"]: " + valores[j] + " -> ³(" + (Math.pow(valores[j], 3)) + ")  \t|\t ²(" + (Math.pow(valores[j], 2)) + ")");
+        }
+        i=0;
+        System.out.println();
+      }
+      
+
+    }
+  }
+}
