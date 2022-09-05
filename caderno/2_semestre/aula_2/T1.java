eecadcd8b201625fbbf22e4e53d00daf06081285
DIEGOHORVATTI <d.horvattid@gmail.com>
2022-09-05 00:58:24 -0400

    fix: aula 1

--- caderno/2_semestre/aula_2/T1.java
+++ caderno/2_semestre/aula_2/T1.java
@@ -5,63 +5,49 @@
  * de maneira decrescente.
 */
 
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
+import java.util.Date;
+import java.util.List;
 
 public class T1 {
   public static void main(String[] args) {
     int TAM_DADOS = 3;
     Random seed = new Random(new Date().getTime());
-
-    int vet1[] = new int[TAM_DADOS];
-    int vet2[] = new int[TAM_DADOS];
-    int vet3[] = new int[TAM_DADOS*2];
+    
+    List<Integer> vet1 = new ArrayList<>();
+    List<Integer> vet2 = new ArrayList<>();
+    List<Integer> vet3 = new ArrayList<>();
 
     //preencher matriz vet1
     for(int i=0; i < TAM_DADOS; i++){
-      vet2[i] = 1+(seed.nextInt((TAM_DADOS*TAM_DADOS)));
+      vet1.add(1 + (seed.nextInt((TAM_DADOS * TAM_DADOS))));
     }
     
     //preencher matriz vet2
     for(int i=0; i < TAM_DADOS; i++){
-      vet1[i] = 1+(seed.nextInt((TAM_DADOS*TAM_DADOS)));
+      vet2.add(1 + (seed.nextInt((TAM_DADOS * TAM_DADOS))));
     }
     
     // preencher matriz vet3 com vet1+vet2
-    int count_1, count_2; count_1=count_2=0;
-    for(int i=0; i < TAM_DADOS*2; i++){
-      if(i % 2 == 0){
-        vet3[i] = vet1[count_1];
-        count_1++;
-      }else{
-        vet3[i] = vet2[count_2];
-        count_2++;
-      }
-    }
+    vet3.addAll(vet1);
+    vet3.addAll(vet2);
 
     // ordenar matriz seguindo o algoritimo de "InsertionSort" vet3
-    int key;
-    int y;
-    for (int j=1; j < vet3.length; j++) {
-      key = vet3[j];
-      for (y = j-1; (y >= 0) && (vet3[y] > key); y--) {
-        vet3[y+1] = vet3[y];
-      }
-      vet3[y+1] = key;
-    }
+    Collections.sort(vet3);
 
     for(int i=0; i < TAM_DADOS; i++){
-      System.out.print("\nvet1["+(i+1)+"]: " + vet1[i]);
+      System.out.print("\nvet1["+(i+1)+"]: " + vet1.get(i));
     }
     System.out.print("\n");
     for(int i=0; i < TAM_DADOS; i++){
-      System.out.print("\nvet2["+(i+1)+"]: " + vet2[i]);
+      System.out.print("\nvet2["+(i+1)+"]: " + vet2.get(i));
     }
 
     System.out.print("\n");
     for(int i=0; i < (TAM_DADOS+TAM_DADOS); i++){
-      System.out.print("\nvet3["+(i+1)+"]: " + vet3[i]);
+      System.out.print("\nvet3["+(i+1)+"]: " + vet3.get(i));
     }
-
   }
 }
