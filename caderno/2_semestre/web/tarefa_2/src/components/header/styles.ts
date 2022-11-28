63ccd9f14a1520fae7953c37cc1912f274b0f7a1
div-leadeasy <diego-leadeasy@outlook.com.br>
2022-11-28 00:21:25 -0400

    add: configuração inicial do styled components

---
+++ caderno/2_semestre/web/tarefa_2/src/components/header/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+
+export const Container = styled.header`
+  width: 100vw;
+  height: 100px;
+  background-color: red;
+  color: width;
+  border: 10px;
+  
+`;
