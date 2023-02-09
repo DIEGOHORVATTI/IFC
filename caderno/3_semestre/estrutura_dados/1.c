9687eb505f4fa6ab6fdf0c84a7e2c46ef80b5a28
DIEGOHORVATTI <d.horvattid@gmail.com>
2023-02-09 10:03:59 -0300

    feat: fim das primieras aulas

---
+++ caderno/3_semestre/estrutura_dados/1.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 10
+#define M 10
+
+// matriz dinamica com ponteiros
+// *(mat + i) -> acesar todos os indices do vetor
+// *(*(mat + i) + j) -> acesar todos os indices de todos os vetores sendo assim uma matriz
+
+/*
+  Dada uma matriz ma(N,M) de inteiros faça um programa em C que:
+  Carregue e escreva a matriz considerando que N e M serão duas constantes de valores 10 e 10, 
+  respectivamente;
+
+  Crie 2 vetores, sl(N) e mc(M), que contenham, respectivamente, as somas dos valores ímpares 
+  das linhas e a média dos valores pares das colunas da matriz ma(N,M). Escreva os vetores de 
+  maneira que o usuário consiga interpretar seu conteúdo adequadamente;
+  Calcule e escreva a diferença entre a diagonal principal e a diagonal secundária da matriz.
+
+	Atenção, para esta questão considerar o uso dos conteúdos:
+  manipulação de vetores e matrizes;
+  modularização e passagem de parâmetros;
+*/
+
+int main(){
+    
+  srand(time(NULL));
+
+  int **ma = malloc(M * sizeof(int*));
+  int *sl = malloc(N * sizeof(int*));
+  int *mc = malloc(M * sizeof(int*));
+
+  for(int i = 0; i < M; i++){
+   *(ma + i) = malloc(N * sizeof(int));
+  }
+
+  for (int j = 0; j < M; j++){
+    for (int i = 0; i < N; i++){
+      *(*(ma + i) + j) = 10+(rand() % 40);
+      *(sl + i) += *(*(ma + i) + j);
+    }
+  }
+
+  for (int j = 0; j < M; j++){
+    for (int i = 0; i < N; i++){
+      printf(" %d", *(*(ma+i) + j) );
+    }
+    printf("\n");
+  }
+
+  for (int i = 0; i < N; i++){
+    printf("\nResultado soma: %d", *( sl + i ) );
+  }
+
+  free(ma);
+  free(sl);
+
+  return(0);
+}
