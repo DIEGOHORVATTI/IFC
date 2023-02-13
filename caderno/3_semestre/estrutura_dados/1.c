a0d32d7794483591fffe53b7cc87656faef6b674
DIEGOHORVATTI <d.horvattid@gmail.com>
2023-02-13 07:47:44 -0300

    atualização: 13-02

--- caderno/3_semestre/estrutura_dados/1.c
+++ caderno/3_semestre/estrutura_dados/1.c
@@ -5,9 +5,22 @@
 #define N 10
 #define M 10
 
-// matriz dinamica com ponteiros
-// *(mat + i) -> acesar todos os indices do vetor
-// *(*(mat + i) + j) -> acesar todos os indices de todos os vetores sendo assim uma matriz
+/*
+ Matriz quadrada => A[n]x[m]
+	n => n = linhas
+	m => colunas
+*/
+
+/* 
+  ! matriz dinamica com ponteiros
+  *(mat + i) -> acesar todos os indices do vetor
+  *(*(mat + i) + j) -> acesar todos os indices de todos os vetores sendo assim uma matriz
+*/
+
+/* 
+  %2 resto 0 = par
+	%2 resto 1 = impar 
+*/
 
 /*
   Dada uma matriz ma(N,M) de inteiros faça um programa em C que:
@@ -15,8 +28,8 @@
   respectivamente;
 
   Crie 2 vetores, sl(N) e mc(M), que contenham, respectivamente, as somas dos valores ímpares 
-  das linhas e a média dos valores pares das colunas da matriz ma(N,M). Escreva os vetores de 
-  maneira que o usuário consiga interpretar seu conteúdo adequadamente;
+  das linhas e a média dos valores pares das colunas da matriz ma(N,M). 
+  Escreva os vetores de maneira que o usuário consiga interpretar seu conteúdo adequadamente;
   Calcule e escreva a diferença entre a diagonal principal e a diagonal secundária da matriz.
 
 	Atenção, para esta questão considerar o uso dos conteúdos:
@@ -38,8 +51,13 @@ int main(){
 
   for (int j = 0; j < M; j++){
     for (int i = 0; i < N; i++){
-      *(*(ma + i) + j) = 10+(rand() % 40);
-      *(sl + i) += *(*(ma + i) + j);
+      *(*(ma + i) + j) = rand() % 4;
+      if( *(*(ma + i) + j) % 2 == 0 ) {
+        *(sl + i) += *(*(ma + i) + j);
+      }
+      if( *(*(ma + i) + j) % 2 != 0 ) {
+        *(mc + j) += *(*(ma + i) + j);
+      }
     }
   }
 
@@ -54,6 +72,12 @@ int main(){
     printf("\nResultado soma: %d", *( sl + i ) );
   }
 
+  printf("\n");
+  for (int i = 0; i < N; i++){
+    printf("\nResultado soma: %d", *( mc + i ) );
+  }
+
+  printf("\n");
   free(ma);
   free(sl);
 
